2c87195326a33b202497585494f35ea0f91faf3a
Michael Bennett <michaeljjbennett@gmail.com>
2018-05-24 01:26:32 -0700

    Added some tests for Javascript imports (and CSS)

---
+++ src/js/load_styles.js
@@ -0,0 +1,17 @@
+var stylesheets = ['main'];
+
+for (var i = 0; i < stylesheets.length; i++) {
+    var id = stylesheets[i] + 'CSSID';
+
+    if (!document.getElementById(id)) {
+        var css_path = 'resources/stylesheets/' + stylesheets[i] + '.css';
+        console.log('Injecting stylesheet: ' + css_path);
+        var link = document.createElement('link');
+        link.id = id;
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = css_path;
+        document.getElementsByTagName('head')[0].appendChild(link);
+    }
+}
+
