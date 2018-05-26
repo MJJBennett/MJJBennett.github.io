fefd5c9fe825cb734726d92b59eba98c01ce16f0
Michael Bennett <michaeljjbennett@gmail.com>
2018-05-26 13:24:31 -0700

    Python script ''complete'', should do for now without any major changes to its functionality. Will allow for dynamic headers. TODO - run it automatically with a git hook.

--- t.py
+++ t.py
@@ -15,7 +15,7 @@ err = print
 def main():
     f = []
     d = []
-    safemode = True
+    safemode = False
     html_dir = './src/html/'
     for (dirpath, dirnames, filenames) in walk(html_dir):
         print("dirpath: " + dirpath + ", dirnames: " + str(dirnames) + ", filenames: " + str(filenames))
@@ -29,12 +29,12 @@ def main():
             for filename in filenames:
                 current_filename = join(current_dir, filename)
                 if isfile(current_filename) and safemode:
-                    print("Warning: " + current_filename + "already exists. Overwrite? (Anything / n)")
+                    print("Warning: " + current_filename + " already exists. Overwrite? (Anything / n)")
                     ans = input()
                     if ans.lower() == 'n':
                         print("Skipping.")
                         continue
-                old_filename = join(html_dir, join(dirpath, filename))
+                old_filename = join(dirpath, filename)
                 print("\nReading " + old_filename)
                 with open(old_filename, 'r') as old_file:
                     file_data = old_file.read()
@@ -45,12 +45,14 @@ def main():
                 for v in variables:
                     with open(variables[v], 'r') as replacement:
                         d = replacement.read()
-                    file_data.replace(v, d)
+                    # print("Replacing " + v + " with:\n" + d + "\nin:\n" + file_data)
+                    file_data = file_data.replace(v, d)
+                    # print("\nResulting in:\n" + file_data)
 
                 with open(current_filename, 'w') as new_file:
                     new_file.write(file_data)
         except:
-            err("Found an exception while dealing with " + join(current_dir, current_filename) + ":")
+            err("Found an exception while dealing with " + current_filename + ":")
             raise
         print("new path: " + dirpath[len(html_dir):] + ".")
 
