5aa68255712fcd415e8908714211784be8022698
Michael Bennett <michaeljjbennett@gmail.com>
2018-05-27 16:12:00 -0700

    Fixed the CSS issues, should be mostly smooth sailing. (Edited GitHub conf to redirect to HTTPS only)

--- src/js/load_styles.js
+++ src/js/load_styles.js
@@ -4,6 +4,7 @@ for (var i = 0; i < stylesheets.length; i++) {
     var id = stylesheets[i] + 'CSSID';
 
     if (!document.getElementById(id)) {
+        //Accesses data at https://mjjbennett.github.io/resources/stylesheets/
         var css_path = 'https://gp.forricide.me/resources/stylesheets/' + stylesheets[i] + '.css';
         console.log('Injecting stylesheet: ' + css_path);
         var link = document.createElement('link');
