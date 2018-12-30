f2f0b7ee77c3796ccb13626d3aad39c48b50689b
Michael Bennett <michaelbennett.dev@gmail.com>
2018-12-29 21:30:03 -0800

    Basic start to the new templater. Already feeling a lot better than the last one.

--- templater.py
+++ templater.py
@@ -6,6 +6,25 @@ New script for templating files.
 Mechanism: Collect file objects, scrape for information, generate results, write out new files.
 """
 
+import os
+
+CONF = {
+    'Directory': './',
+    'Verbose': 'True'
+}
+
+class Configuration:
+    def __init__(self, config_dict):
+        self.directory = self.default(config_dict, 'Directory', './')
+        self.do_write = self.default(config_dict, 'Verbose', 'False').lower() == 'true'
+
+    def default(self, d, k, default):
+        return default if k not in d else d[k]
+
+    def write(self, *args, **kwargs):
+        if self.do_write:
+            print(*args, **kwargs)
+
 class FileObject:
     """
     Class for abstracting away file object logic.
@@ -25,6 +44,45 @@ class FileObject:
     def foreach_replace(self, Function):
         self.filedata = [ Function(x) for x in self.filedata ]
 
+class Collector:
+    """
+    This class is used to collect all the filenames to template.
+    """
+    def __init__(self, configuration):
+        self.config = configuration
+        self.variables = {}
+        self.files = []
 
-if __name__ = "__main__":
+    def collect_files(self):
+        self.config.write('Collecting files from directory:', self.config.directory)
+        directory = self.config.directory
+
+        for (filenames, _, _) in os.walk(directory):
+            print(filenames)
+
+    def files(self):
+        return self.files
+
+    def variables(self):
+        return self.variables
+        
+
+if __name__ == "__main__":
     # Running the templating engine
+
+    # Create and parse the configuration
+    config = Configuration(CONF)
+
+    # Create a collector; find all files that need to be generated
+    collector = Collector(config)
+    collector.collect_files()
+
+    # Create the fileobjects to wrap the filenames for the template files
+    file_objects = []
+    for filename in collector.files():
+        file_objects.append(FileObject(filename, config.new_filename(filename)))
+    # We might have encountered a variables file
+    variables = collector.variables()
+
+    # We now have the files, generate the output
+    # for file_object in file_objects:
